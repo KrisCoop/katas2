72fe6aae403720fa5845d9619baf3f2d5919d9a6
Kris Cooper <prymaldesign@gmail.com>
2019-11-19 18:29:27 -0700

    kata

---
+++ 8kyu_Enumerable Magic 25 - Take the First N Elements.js
@@ -0,0 +1,20 @@
+// Create a method take that accepts a list/array and a number n, and returns a list/array array 
+// of the first n elements from the list/array.
+
+// If you need help, here's a reference:
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice
+
+function take(arr, n) {
+    // loop through the array with a standard for loop,
+    // only loop to n.length
+
+    
+    
+    // push each element (up to n) into a new array
+
+
+    // return the new array
+
+
+  }
