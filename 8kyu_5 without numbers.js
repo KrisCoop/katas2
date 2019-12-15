2d23f62555f646c9af2da3917b4a2aea757ac029
Kris Cooper <prymaldesign@gmail.com>
2019-12-14 20:16:20 -0700

    kata

---
+++ 8kyu_5 without numbers.js
@@ -0,0 +1,14 @@
+// Write a function that always returns 5
+
+// Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+// Good luck :)
+
+function unusualFive() {
+
+    let arr = ['a','a','a','a','a']
+    console.log(arr.length)
+    return arr.length
+  }
+
+  console.log(unusualFive())
