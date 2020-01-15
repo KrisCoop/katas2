1cde68702bead48234ecd89cb0b2e4184f34431d
Kris Cooper <prymaldesign@gmail.com>
2020-01-15 14:05:53 -0700

    kata update

---
+++ 7kyu_Shortest_Word.js
@@ -0,0 +1,25 @@
+// Simple, given a string of words, return the length of the shortest word(s).
+
+// String will never be empty and you do not need to account for different data types.
+
+function findShort(s){
+
+    let arr1 = s.split(' ')
+    console.log(arr1)
+
+    let shortest = arr1.reduce(function(lil, current){
+        if (lil.length < current.length) {
+            return lil;
+        } else if (lil.length === current.length) {
+            return lil;
+        } else {
+            return current;
+        }
+    });
+    console.log(shortest)
+    return shortest.length
+}
+
+
+console.log(findShort("bitcoin take over the world maybe who knows perhaps"), 3);
+// console.log(findShort("turns out random test cases are easier than writing out basic ones"), 3); 
