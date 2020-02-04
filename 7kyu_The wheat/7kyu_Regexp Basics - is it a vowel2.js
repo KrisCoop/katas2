d661268ec5b164e5e71cdfa0c64dd57563bc207f
Kris Cooper <prymaldesign@gmail.com>
2020-02-03 17:33:59 -0700

    kata

--- 7kyu_The wheat/7kyu_Regexp Basics - is it a vowel2.js
+++ 7kyu_The wheat/7kyu_Regexp Basics - is it a vowel2.js
@@ -2,15 +2,23 @@
 
 String.prototype.vowel = function() {
 
-    
+    // let param = this.toLowerCase()
+    // console.log(param)
+    // let ans = false
 
-    console.log(this)
+    // if (param === 'a' || param === 'e' || param === 'i' || param === 'o' || param === 'u' || param === 'y'){ans = true}
+    // return ans
+
+    // return ['a', 'e', 'i', 'o', 'u'].indexOf(this.toLowerCase()) !== -1
+    //this worked, but it's not a regular expression
+
+    return (/^[aeiou]$/i).test(this.toLowerCase())
     
 }
 
-// console.log(''.vowel(), false);
-// console.log('a'.vowel(), true);
+console.log(''.vowel(), false);
+console.log('a'.vowel(), true);
 console.log('E'.vowel(), true);
-// console.log('ou'.vowel(), false);
-// console.log('z'.vowel(), false);
-// console.log('lol'.vowel(), false);
+console.log('ou'.vowel(), false);
+console.log('z'.vowel(), false);
+console.log('lol'.vowel(), false);
